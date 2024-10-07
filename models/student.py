c23575479b8ada37470d44ce7b773d68bde52a2f
rekebuba <abukiabdullahi1@gmail.com>
2024-10-07 23:06:50 +0300

    A custom student id generator for Student table

--- models/student.py
+++ models/student.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-
 from sqlalchemy import Column, Integer, String, ForeignKey, CheckConstraint, Float
 from models.engine.db_storage import BaseModel, Base
+from ethiopian_date import EthiopianDateConverter
+import random
+from datetime import datetime
 
 
 class Student(BaseModel, Base):
     __tablename__ = 'students'
+    id = Column(String(120), primary_key=True, unique=True)
     first_name = Column(String(50), nullable=False)
     father_name = Column(String(50), nullable=False)
     g_father_name = Column(String(50))
@@ -30,3 +33,31 @@ class Student(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes score"""
         super().__init__(*args, **kwargs)
+        self.id = self.generate_student_id(kwargs['grade'])
+
+
+    def generate_student_id(self, grade):
+        """Function to generate a custom student ID based on the grade"""
+        id = ''
+        section = ''
+        if grade < 8:
+            section = 'ELM'
+        elif grade > 8 and grade < 11:
+            section = 'HIG'
+        else:
+            section = 'PRE'
+        unique = True
+        while unique:
+            num = random.randint(1000, 9999)
+            starting_year = EthiopianDateConverter.date_to_ethiopian(datetime.now()).year % 100
+            id = f'{section}/{num}/{starting_year}/{grade}'
+            if not self.id_exists(id):
+                unique = False
+        return id
+
+    def id_exists(self, id):
+        """Function to check if the student ID already exists"""
+        from models import storage
+        if storage._DBStorage__session.query(Student).filter_by(id=id).first():
+            return True
+        return False
