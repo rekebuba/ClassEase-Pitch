e53d1eef043bc15e42082350aa634b3e326402bd
rekebuba <abukiabdullahi1@gmail.com>
2025-05-14 23:57:08 +0300

    refactor(docker): Update Dockerfile to correct working directory and environment variable paths for better consistency
    test: Change app fixture in tests to use testing configuration instead of development

--- app/backend/config.py
+++ app/backend/config.py
@@ -22,14 +22,23 @@ class Config:
 class DevelopmentConfig(Config):
     IS_DOCKER = os.environ.get("IS_DOCKER", "false").lower() == "true"
 
-    user = os.getenv("KEY_MYSQL_USER")
-    password = os.getenv("KEY_MYSQL_PWD")
-    host = "mysql_container" if IS_DOCKER else "localhost"
-    db = os.getenv("KEY_MYSQL_DB")
-    if not all([user, password, host, db]):
+    user = os.getenv("DEV_MYSQL_USER")
+    password = os.getenv("DEV_MYSQL_PWD")
+    host = (
+        os.getenv("MYSQL_DOCKER_HOST")
+        if IS_DOCKER
+        else os.getenv("MYSQL_LOCAL_HOST")
+    )
+    port = (
+        os.getenv("MYSQL_DOCKER_PORT")
+        if IS_DOCKER
+        else os.getenv("MYSQL_LOCAL_PORT")
+    )
+    db = os.getenv("DEV_MYSQL_DB")
+    if not all([user, password, host, port, db]):
         raise ValueError("Missing required environment variables for db connection")
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = f"mysql://{user}:{password}@{host}/{db}"
+    SQLALCHEMY_DATABASE_URI = f"mysql://{user}:{password}@{host}:{port}/{db}"
 
 
 class ProductionConfig(Config):
@@ -39,13 +48,28 @@ class ProductionConfig(Config):
 class TestingConfig(Config):
     """Testing configuration."""
 
+    IS_DOCKER = os.environ.get("IS_DOCKER", "false").lower() == "true"
+
     TESTING = True
     DEBUG = True
     user = os.getenv("TEST_MYSQL_USER")
     password = os.getenv("TEST_MYSQL_PWD")
-    host = os.getenv("TEST_MYSQL_HOST")
+    host = (
+        os.getenv("MYSQL_DOCKER_HOST")
+        if IS_DOCKER
+        else os.getenv("MYSQL_LOCAL_HOST")
+    )
+    port = (
+        os.getenv("MYSQL_DOCKER_PORT")
+        if IS_DOCKER
+        else os.getenv("MYSQL_LOCAL_PORT")
+    )
     db = os.getenv("TEST_MYSQL_DB")
-    SQLALCHEMY_DATABASE_URI = f"mysql://{user}:{password}@{host}/{db}"
+
+    if not all([user, password, host, port, db]):
+        raise ValueError("Missing required environment variables for db connection")
+
+    SQLALCHEMY_DATABASE_URI = f"mysql://{user}:{password}@{host}:{port}/{db}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Overriding the JWT secret keys for testing
