f24c1502fc95863877aef6ef818edd0de9836c40
rekebuba <abukiabdullahi1@gmail.com>
2026-03-20 13:31:18 +0300

    feat: Updated API endpoint URLs to remove trailing slashes for consistency.
    - Adjusted healthcheck commands in Docker Compose files to match updated endpoint URLs.
    - Keep using sync for Alembic

--- app/frontend/src/client/client.gen.ts
+++ app/frontend/src/client/client.gen.ts
@@ -14,5 +14,5 @@ import { type Config, type ClientOptions as DefaultClientOptions, createClient, 
 export type CreateClientConfig<T extends DefaultClientOptions = ClientOptions> = (override?: Config<DefaultClientOptions & T>) => Config<Required<DefaultClientOptions> & T>;
 
 export const client = createClient(createConfig<ClientOptions>({
-    baseURL: 'http://localhost:8000'
+    baseURL: 'http://localhost:8080'
 }));
